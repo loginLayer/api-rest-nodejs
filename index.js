173ed75ab442836e6b6ebf122dfa71a071513432
loginLayer <84354397+loginLayer@users.noreply.github.com>
2025-07-24 00:13:23 -0300

    Mejoras API

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import notFound from "./src/middlewares/not-found.js";
+import maintenance from "./src/middlewares/maintenance.js"
+import productsRouter from "./src/routes/products.router.js";
+import authRouter from "./src/routes/auth.router.js";
+
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); 
 
 app.get("/", (req, res) => {
     res.json({ message :  "Bienvenido a la API REST"});
 });
 
-import productsRouter from "./src/routes/products.router.js";
 app.use('/api/v1', productsRouter);
 
-import authRouter from "./src/routes/auth.router.js";
-app.use(authRouter);
-
-import notFound from "./src/middlewares/not-found.js";
-
-import maintenance from "./src/middlewares/maintenance.js"
 app.get("/maintance", (req, res) => {
     res.json({ message :  "API en mantenimiento"});
 });
+
+app.use(authRouter);
 
 app.use(notFound);
 
